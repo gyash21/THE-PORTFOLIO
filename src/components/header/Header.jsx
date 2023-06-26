e74554c9e4015720679ddb9aaf93137928cce2c2
Yash Gupta <64211142+gyash21@users.noreply.github.com>
2023-06-26 14:32:51 +0530

    header done with responsive toggle and menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import React, {useState} from "react";
+import "./header.css";
 
 const Header = () => {
+/*=============Toggle Menu=============*/
+  const [Toggle, showMenu] = useState(false);
+
   return (
     <header className="header">
       <nav className="nav container">
         <a href="index.html" className="nav__logo">Gupta</a>
 
-        <div className="nav__menu">
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
               <a href="#home" className="nav__link active-link">
@@ -50,9 +55,9 @@ const Header = () => {
               </a>
             </li>
           </ul>
-          <i className="uil uil-times nav__close"></i>
+          <i className="uil uil-times nav__close" onClick = {() => showMenu(!Toggle)}></i>
         </div>
-          <div className="nav__toggle">
+          <div className="nav__toggle" onClick = {() => showMenu(!Toggle)}>
           <i class="uil uil-apps"></i>
           </div>
       </nav>
