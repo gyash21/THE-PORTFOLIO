eed93df86ab5a34de07b4e33f635dc3cce04597a
Yash Gupta <64211142+gyash21@users.noreply.github.com>
2023-06-27 15:13:16 +0530

    added home and social component

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Data = () => {
+    return(
+       
+        <div className="home__data">
+            <h1 className="home__tile">Yash Gupta</h1>
+        </div>
+    )
+}
+
+
+
+export default Data
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Social = () => {
+    return(
+       <div className="home__social">
+        <a href="https://instagram.com/decode.yash" className="home__social-icon" target="_blank"><i class="uil uil-instagram"></i></a>
+
+        <a href="https://yashgupta.blog" className="home__social-icon" target="_blank"><i class="uil uil-dribbble"></i></a>
+
+        <a href="https://github.com/gyash21" className="home__social-icon" target="_blank"><i class="uil uil-github-alt"></i></a>
+       </div>
+    )
+}
+
+
+
+export default Social
