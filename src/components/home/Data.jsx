4f04bd7cf250982b628cd24de2f71b36a158c306
Yash Gupta <64211142+gyash21@users.noreply.github.com>
2023-06-28 15:10:16 +0530

    fixed home title

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -4,7 +4,7 @@ const Data = () => {
     return(
        
         <div className="home__data">
-            <h1 className="home__tile">Yash Gupta
+            <h1 className="home__title">Yash Gupta
             
             <svg
                   width="36"
