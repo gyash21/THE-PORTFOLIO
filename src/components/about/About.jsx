45fe23a9ca08bc67e1eaa4a6f0f4a3d0a20eb789
Yash Gupta <64211142+gyash21@users.noreply.github.com>
2023-07-25 16:57:57 +0530

    resume update in about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/resume.pdf";
+import CV from "../../assets/Yash-Resume.pdf";
 import Info from "./Info";
 
 const About = () => {
