f1354888b9f5a245c4580496eb598057cf33a5ae
Yash Gupta <64211142+gyash21@users.noreply.github.com>
2023-07-25 15:05:08 +0530

    class name corrected

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -8,21 +8,21 @@ const Info = () => {
         <i class='bx bx-award about__icon'></i>
 
           <h3 className="about__title">Experience</h3>
-          <span className="about__title">1+ Year</span>
+          <span className="about__subtitle">1+ Year</span>
         </div>
 
         <div className="about__box">
         <i class='bx bx-briefcase-alt about__icon' ></i>
 
           <h3 className="about__title">Completed</h3>
-          <span className="about__title">5+ Projects</span>
+          <span className="about__subtitle">5+ Projects</span>
         </div>
 
         <div className="about__box">
         <i class='bx bx-support about__icon' ></i>
 
           <h3 className="about__title">Support</h3>
-          <span className="about__title">Online (Personalised)</span>
+          <span className="about__subtitle">Online (Personalised)</span>
         </div>
       </div>
     )
