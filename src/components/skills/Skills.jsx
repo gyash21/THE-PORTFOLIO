7482d34cd104157931182811eb4fc330c906aa9a
Yash Gupta <gyash21@gmail.com>
2023-07-26 00:30:42 +0530

    added some frontend skills element

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import "./Frontend";
+import "./skills.css"
+import Frontend from './Frontend';
+import Backend from './Backend';
 
 const Skills = () => {
   return (
     <section className="skills section" id="skills">
         <h2 className="section__title">Skills</h2>
       <span className="section__subtitle">My Tech Forte</span>
+
+      <div className="skills__container container grid">
+        <Frontend/>
+        <Backend/>
+      </div>
     </section>
   )
 }
