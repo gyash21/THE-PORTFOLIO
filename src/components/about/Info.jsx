19df62b1e8095871368ea2aca7dff55bc2aee5d1
Yash Gupta <64211142+gyash21@users.noreply.github.com>
2023-07-25 15:00:24 +0530

    added info details

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,5 +1,32 @@
+<<<<<<< HEAD
 const Info = () => {
     return 
+    (
+      <div className="about__info grid">
+>>>>>>> 957405fd0fac5eb208a4dd497dff0dee0d273dfb
+
+        <div className="about__box">
+        <i class='bx bx-award about__icon'></i>
+
+          <h3 className="about__title">Experience</h3>
+          <span className="about__title">1+ Year</span>
+        </div>
+
+        <div className="about__box">
+        <i class='bx bx-briefcase-alt about__icon' ></i>
+
+          <h3 className="about__title">Completed</h3>
+          <span className="about__title">5+ Projects</span>
+        </div>
+
+        <div className="about__box">
+        <i class='bx bx-support about__icon' ></i>
+
+          <h3 className="about__title">Support</h3>
+          <span className="about__title">Online (Personalised)</span>
+        </div>
+      </div>
+    )
   }
   
   export default Info
