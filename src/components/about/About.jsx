3e60fba4d67d0a7c6ca08577c1c5489ea632f763
Yash Gupta <64211142+gyash21@users.noreply.github.com>
2023-07-25 12:30:49 +0530

    added the data on about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import "./about.css";
+import AboutImg from "../../assets/about.img";
+import CV from "../../assets/resume.pdf"
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className="about section" id="about">
+      <h2 className="section__title">About Me</h2>
+      <span className="section__subtitle">My Introduction</span>
+
+      <div className="about__container container grid">
+<img src={AboutImg} alt="" className="about__img" />
+
+<div className="about__data"></div>
+      </div>
+    </section>
   )
 }
 
 export default About
+
