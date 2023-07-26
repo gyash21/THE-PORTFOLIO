403d3ea25087dacf156598b7b16a43c23cba53ac
Yash Gupta <64211142+gyash21@users.noreply.github.com>
2023-07-26 10:40:51 +0530

    item removed from skills box

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -60,15 +60,6 @@ const Frontend = () => {
             </div>
             </div>
             
-
-            <div className="skills__data">
-            <i class='bx bx-badge-check'></i>
-            <div>
-                <h3 className="skills__name">Typescript</h3>
-                <span className="skills__level">Intermediate</span>
-            </div>
-            </div>
-
         </div>
         </div>   
    </div>
