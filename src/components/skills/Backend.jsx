691f58ba05c6229156b639791d099ff208ba56b0
Yash Gupta <64211142+gyash21@users.noreply.github.com>
2023-07-26 17:09:10 +0530

    skills box divided into two groups

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -11,7 +11,7 @@ const Backend = () => {
             <div className="skills__data">
             <i class='bx bx-badge-check'></i>
             <div>
-                <h3 className="skills__name">Node JS</h3>
+                <h3 className="skills__name">NodeJS</h3>
                 <span className="skills__level">Intermediate</span>
             </div>
             </div>
@@ -35,6 +35,7 @@ const Backend = () => {
             </div>
 
 
+            <div className="skills__group">
             <div className="skills__data">
             <i class='bx bx-badge-check'></i>
             <div>
