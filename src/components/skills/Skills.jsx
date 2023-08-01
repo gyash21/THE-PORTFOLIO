cd9c3c5be4b905c5583f2514c0b24ba78305da6f
Yash Gupta <gyash21@gmail.com>
2023-08-01 21:29:00 +0530

    update - added a services section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -7,7 +7,7 @@ import Backend from './Backend';
 const Skills = () => {
   return (
     <section className="skills section" id="skills">
-        <h2 className="section__title">Skills</h2>
+      <h2 className="section__title">Skills</h2>
       <span className="section__subtitle">My Tech Forte</span>
 
       <div className="skills__container container grid">
