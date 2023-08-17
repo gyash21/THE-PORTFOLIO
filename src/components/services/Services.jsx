18cb742374d489f3084d54a970bc83bdb8897b76
Yash Gupta <64211142+gyash21@users.noreply.github.com>
2023-08-17 16:15:03 +0530

    update- added different element in services page

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -8,23 +8,139 @@ const Services = () => {
 
 
 
-      <section__container className="container grid">
+      <div className="services__container container grid">
         <div className="services__content">
           <div>
-            <i className="uil uil services__icon"></i>
+            <i className="uil uil-web-grid services__icon"></i>
              <h3 className="services__title">
-              
+              Product <br />Designer
               </h3> 
             
           </div>
 
-          <span className="services__button"> View More {" "} <i className="uil uil-arrow-right services__button-icon"></i></span>
+          <span className="services__button"> View More<i className="uil uil-arrow-right services__button-icon"></i></span>
 
 
           <div className="services__modal">
             <div className="services__modal-content">
               <i className="uil uil times services__modal-close">
 
+              </i>
+
+              <h3 className="services__modal-title">
+              Product Designer
+              </h3>
+              <p className="services__modal-description">Providing quality and exceptional work to clients and companies</p>
+
+              <ul className="service__modal services grid">
+                <li className="services__modal-services">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">Develops UX/UI Interface</p>
+                </li>
+
+
+                <li className="services__modal-services">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">Web App Development</p>
+                </li>
+
+
+                <li className="services__modal-services">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">Design and Mockup</p>
+                </li>
+
+
+                <li className="services__modal-services">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">Brand Identity</p>
+                </li>
+
+
+                <li className="services__modal-services">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">Content Marketing</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="services__content">
+          <div>
+            <i className="uil uil-arrow services__icon"></i>
+             <h3 className="services__title">
+              UI/UX <br /> Designer
+              </h3> 
+            
+          </div>
+
+          <span className="services__button"> View More<i className="uil uil-arrow-right services__button-icon"></i></span>
+
+
+          <div className="services__modal">
+            <div className="services__modal-content">
+              <i className="uil uil times services__modal-close">
+
+              </i>
+
+              <h3 className="services__modal-title">
+              UI/UX Designer
+              </h3>
+              <p className="services__modal-description">Providing quality and exceptional work to clients and companies</p>
+
+              <ul className="service__modal services grid">
+                <li className="services__modal-services">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">Develops UX/UI Interface</p>
+                </li>
+
+
+                <li className="services__modal-services">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">Web App Development</p>
+                </li>
+
+
+                <li className="services__modal-services">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">Design and Mockup</p>
+                </li>
+
+
+                <li className="services__modal-services">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">Brand Identity</p>
+                </li>
+
+
+                <li className="services__modal-services">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">Content Marketing</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="services__content">
+          <div>
+            <i className="uil uil-edit services__icon"></i>
+             <h3 className="services__title">
+              Visual <br /> Designer
+              </h3> 
+            
+          </div>
+
+          <span className="services__button"> View More<i className="uil uil-arrow-right services__button-icon"></i></span>
+
+
+          <div className="services__modal">
+            <div className="services__modal-content">
+              <i className="uil uil times services__modal-close">
+              Visual Designer
               </i>
 
               <h3 className="services__modal-title">
@@ -65,10 +181,9 @@ const Services = () => {
             </div>
           </div>
         </div>
-      </section__container>
-
+</div>
     </section>
-  )
-}
+  );
+};
 
 export default Services
