f774c7ee88815b8256e8c752579f9911d82cc070
Yash Gupta <gyash21@gmail.com>
2023-08-21 23:31:18 +0530

    update- added modal open functionality

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./services.css";
 
 const Services = () => {
+  const [toggleState, setToggleState] = useState(0);
+
+  const toggleTab = (index) => {
+    setToggleState(index)
+  }
+
   return (
     <section className="services section" id="services">
         <h2 className="section__title">Services</h2>
@@ -19,10 +25,10 @@ const Services = () => {
             
           </div>
 
-          <span className="services__button"> View More<i className="uil uil-arrow-right services__button-icon"></i></span>
+          <span className="services__button" onClick={() => toggleTab(1)}> View More<i className="uil uil-arrow-right services__button-icon"></i></span>
 
 
-          <div className="services__modal">
+          <div className={toggleState === 1 ? "services__modal active-modal" : "services__modal"}>
             <div className="services__modal-content">
               <i className="uil uil-times services__modal-close">
 
