e0e95205c309f4c004092e6d9e60e1d20ed38d75
Yash Gupta <gyash21@gmail.com>
2023-08-22 23:58:09 +0530

    update- pop-up and close in modal 3

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -141,10 +141,10 @@ const Services = () => {
             
           </div>
 
-          <span className="services__button"> View More<i className="uil uil-arrow-right services__button-icon"></i></span>
+          <span onClick={() => toggleTab(3)} className="services__button"> View More<i className="uil uil-arrow-right services__button-icon"></i></span>
 
 
-          <div className="services__modal">
+          <div className={toggleState === 3 ? "services__modal active-modal" : "services__modal"}>
             <div className="services__modal-content">
               <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close">
              
