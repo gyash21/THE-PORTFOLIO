bd2aedd65b2468e27fb48727688cc715885db926
Yash Gupta <gyash21@gmail.com>
2023-08-23 23:57:37 +0530

    update- adding qualification elements

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -37,15 +37,42 @@ const Qualification  = () => {
                 <div className="qualification__content">
                     <div className="qualification__data">
                         <div>
-                            <h3 className="qualification__title"></h3>
-                            <span className="qualification__subtitle"></span>
+                            <h3 className="qualification__title">Web Design</h3>
+                            <span className="qualification__subtitle">Spain-Institute</span>
                             <div className="qualification__calender">
 
                                 <i className="uil uil-calender-alt">
-                                    
-                                </i>
+                                </i> 2021 - Present
                             </div>
                         </div>
+
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+
+                            <div className="qualification__line"></div>
+                        </div>
+                    </div>
+                    <div></div>
+
+
+                    <div>
+                            <span className="qualification__rounder"></span>
+
+                            <span className="qualification__line"></span>
+                        </div>
+
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Art Director</h3>
+                            <span className="qualification__subtitle">Spain-Institute</span>
+                            <div className="qualification__calender">
+
+                                <i className="uil uil-calender-alt">
+                                </i> 2021 - Present
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
