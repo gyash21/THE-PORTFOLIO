ee42c62dbe5ab267396ac443884d01ea696b39b9
Yash Gupta <gyash21@gmail.com>
2023-08-31 23:53:13 +0530

    update- 3 educational sections added

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -117,6 +117,75 @@ const Qualification  = () => {
 
                     
                 </div>
+
+
+
+
+
+
+                <div className="qualification__content">
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Web Design</h3>
+                            <span className="qualification__subtitle">Spain-Institute</span>
+                            <div className="qualification__calender">
+
+                                <i className="uil uil-calender-alt">
+                                </i> 2021 - Present
+                            </div>
+                        </div>
+
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+
+                            <div className="qualification__line"></div>
+                        </div>
+                    </div>
+                    <div></div>
+
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Art Director</h3>
+                            <span className="qualification__subtitle">Spain-Institute</span>
+                            <div className="qualification__calender">
+
+                                <i className="uil uil-calender-alt">
+                                </i> 2021 - Present
+                            </div>
+                        </div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+
+                            <span className="qualification__line"></span>
+                        </div>
+
+                    </div>
+
+
+
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Web Development</h3>
+                            <span className="qualification__subtitle">Spain-Institute</span>
+                            <div className="qualification__calender">
+
+                                <i className="uil uil-calender-alt">
+                                </i> 2021 - Present
+                            </div>
+                        </div>
+
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+
+                            <div className="qualification__line"></div>
+                        </div>
+                    </div>
+                    <div></div>
+
+                    
+                </div>
             </div>
         </div>
         </section>
