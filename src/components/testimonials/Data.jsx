3d44eb01357130a60fbdf270a5a8f3001b8e8e1e
Yash Gupta <gyash21@gmail.com>
2023-09-10 23:34:55 +0530

    update- objects for testimonial

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -1,3 +1,16 @@
 import Image1 from "../../assets/testimonial1.png";
 import Image2 from "../../assets/testimonial2.png";
 import Image3 from "../../assets/testimonial3.png";
+
+
+
+export const Data =[
+    {
+        id:1,
+        image: Image1,
+        title: "Harvey Spectre",
+        description:
+            "An impressive and neat job by Yash, covering all our needs and thus creating an amazing and super-fast web app"
+
+    }
+]
