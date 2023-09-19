fcad896035376c6941ef21c16a8f7abd54c21e61
Yash Gupta <gyash21@gmail.com>
2023-09-19 23:48:28 +0530

    update- contact page sections

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className="contact section" id="contact">
+        <h2 className="section__title">Get in Touch</h2>
+        <span className="section__subtitle">Contact Me</span>
+
+
+        <div className="contact__container container grid">
+            <div className="contact__content">
+                <h3 className="contact__title">
+
+                </h3>
+            </div>
+
+            <div className="contact__content">
+                <h3 className="contact__title">
+                    
+                </h3>
+            </div>
+        </div>
+
+    </section>
   )
 }
 
