6a5e24eed43d05f93c4a4c8e8b8cffad005fe6d5
Yash Gupta <gyash21@gmail.com>
2023-09-26 14:13:35 +0530

    update-added footer component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
     <Skills/>
     <Services/>
     <Contact/>
+    <Footer/>
     </main>
     </>
   )
