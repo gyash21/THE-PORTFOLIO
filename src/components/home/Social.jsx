4a3a184689f153e9d0747b27af8e48ef9ff4c056
Yash Gupta <gyash21@gmail.com>
2024-02-19 22:02:10 +0530

    update- more in social icons

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Social = () => {
     return(
        <div className="home__social">
-        <a href="https://instagram.com/decode.yash" className="home__social-icon" target="_blank"><i class="uil uil-instagram"></i></a>
+        <a href="https://instagram.com/decode.yash" className="home__social-icon" target="_blank"><i class="uil uil-instagram-alt"></i></a>
 
         <a href="https://twitter.com/gyash21" className="home__social-icon" target="_blank"><i class="uil uil-twitter"></i></a>
 
-        <a href="https://github.com/gyash21" className="home__social-icon" target="_blank"><i class="uil uil-github-alt"></i></a>
+        <a href="https://github.com/gyash21" className="home__social-icon" target="_blank"><i class="uil uil-github"></i></a>
        </div>
     );
 };
