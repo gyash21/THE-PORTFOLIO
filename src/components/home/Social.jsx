c906357562d8d34797b1839f101d069da3ac0dfd
Yash Gupta <gyash21@gmail.com>
2024-02-19 21:59:41 +0530

    update- twitter update

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -5,7 +5,7 @@ const Social = () => {
        <div className="home__social">
         <a href="https://instagram.com/decode.yash" className="home__social-icon" target="_blank"><i class="uil uil-instagram"></i></a>
 
-        <a href="https://yashgupta.blog" className="home__social-icon" target="_blank"><i class="uil uil-dribbble"></i></a>
+        <a href="https://twitter.com/gyash21" className="home__social-icon" target="_blank"><i class="uil uil-twitter"></i></a>
 
         <a href="https://github.com/gyash21" className="home__social-icon" target="_blank"><i class="uil uil-github-alt"></i></a>
        </div>
